bcc68e24d99152ef915cc10efc6b6329eca3cc59
David Sevilla Martin <dsevilla192@icloud.com>
2016-08-10 12:40:41 -0400

    added source code link in info command

--- Commands/Info.js
+++ Commands/Info.js
@@ -2,7 +2,8 @@ const InfoMessage = [
   '**INFO**',
   '',
   `I was made by <@!175008284263186437>. I was created because tons of people went to the <#81385020756865024> channel in the Discord API server to get help creating a bot while they themselves had no idea how to use JavaScript.`,
-  'I was also created to get rid of music bots :slight_smile: and help the chat providing docs links, and resources to learn Javascript, DiscordJS, and Eris.'
+  'I was also created to get rid of music bots :slight_smile: and help the chat providing docs links, and resources to learn Javascript, DiscordJS, and Eris.',
+  'You can see the source code at <https://github.com/datitisev/DiscordBot-OhPlease>'
 ].join('\n');
 
 module.exports = (bot, DeleteMessageCommand) => {
