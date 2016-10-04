c1378042d152703bc63ab07a399143aa08480e66
David Sevilla Martin <dsevilla192@icloud.com>
2016-10-04 17:34:22 -0400

    updated logger

--- log.js
+++ log.js
@@ -3,76 +3,116 @@
 const chalk = require('chalk');
 const EventEmitter = require("events").EventEmitter;
 const moment = require('moment');
-const util = require('util')
+const util = require('util');
 const icons = {
-  error: '🔥   ',
+  error: '🔥  ',
   debug: '⚙  ',
   info: '🆗   ',
-  message: '💁',
-  warn: '⚠ '
+  message: '💁'
 };
 let socket;
 let logs = [];
 
+/**
+ * A Logger to replace `console.log`.
+ * It logs to the console, and web
+ */
 class Logger {
 
-  constructor(logLevel) {
+  constructor() {
     this.emitter = new EventEmitter();
-    this.logLevel = 'debug';
 
     this.debug = this.debug.bind(this);
     this.info = this.info.bind(this);
     this.error = this.error.bind(this);
     this.message = this.message.bind(this);
-    this.verbose = this.verbose.bind(this);
-    this.warn = this.warn.bind(this);
+    this.github = this.github.bind(this);
   }
 
-  // methods
-  debug(msg) {
-    this.log('debug', msg);
+  /**
+   * Debug it like you would `console.log` it ;)
+   * @param {Mixed} ...args - all the arguments
+   */
+  debug(...args) {
+    this.log('debug', ...args);
   }
-  info(msg) {
-    this.log('info', msg);
+
+  /**
+   * Log something about github, self-explanatory (right?)
+   * @param {Mixed} ...args - all the arguments
+   */
+  github(...args) {
+    this.log('github', ...args);
   }
+
+  /**
+   * Give me some info, pls
+   * @param {Mixed} ...args - all the arguments
+   */
+  info(...args) {
+    this.log('info', ...args);
+  }
+
+  /**
+   * You gotta error it to me ;((
+   * @param {Mixed} ...args - all the arguments
+   */
   error(msg) {
     if (typeof msg == 'object' && msg.stack) {
-      msg.stack = msg.stack.replace(process.cwd(), '.');
+      msg.stack = msg.stack.replace(__dirname, './').replace(new RegExp(TOKEN, 'g'), 'TOKEN_WAS_HERE');
     }
     this.log('error', msg);
   }
+
+  /**
+   * Log a message!
+   * @param {Mixed} ...args - all the arguments
+   */
   message(msg) {
     this.log('message', msg);
   }
-  verbose(msg) {
-    this.log('verbose', msg);
-  }
-  warn(msg) {
-    this.log('warn', msg);
-  }
+
+  /**
+   *
+   * @param {String} event - what kind of log
+   * @param {cb} cb - callback
+   */
   on(event, cb) {
     this.emitter.on(event, cb);
   }
 
   // inside
 
+  /**
+   * Log, with the level and the message
+   * @param {String} level - log level, i.e: error
+   * @param {Mixed} message - actual message, ya know?
+   * @private
+   */
   log(level, msg) {
+    const TOKEN = process.env.DISCORD_TESTING_BOT_TOKEN || process.env.TOKEN;
     let args = [...arguments].slice(1);
-    let message = util.format(...args).replace(process.env.TOKEN, 'TOKEN');
-    let log = `[${moment().format("MM/D/YY HH:mm:ss")}] ${icons[level] || ' '} ${level}: ${message}`;
+    let message = util.format(...args).replace(new RegExp(TOKEN, 'g'), 'TOKEN_WAS_HERE');
 
-    if (this.logLevel === 'error' && level !== 'error') return false;
-    if (this.logLevel !== 'verbose' && level === 'verbose') return false;
+    let log = `[${moment().format("MM/D/YY HH:mm:ss")}] ${icons[level]} ${level}: ${util.format(...args)}`;
 
     console.log(log);
 
     this.emit(log);
   }
 
+  /**
+   * Get all the logs from the latest run
+   * @return {Array} logs - Logs, hhm..
+   */
   get logs() {
     return logs;
   }
 
+  /**
+   * Emit to socket.io and push to the logs
+   * @private
+   */
   emit(log) {
     logs.push(log.replace(/(\[)\w+(m)/g, ''));
     if (!socket) return false;
